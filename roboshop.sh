acafc16c7bc5d0ffcebcb2840f07bb70c9640782
prasad <guruprasadbolisetty24@.com>
2025-06-17 13:59:31 +0530

    hello world started

--- roboshop.sh
+++ roboshop.sh
@@ -3,8 +3,8 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-06776cce51381651f" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
-DOMAIN_NAME="daws84s.site" # replace with your domain
+ZONE_ID="Z0193002ZK4VQFXB1ARL" # replace with your ZONE ID
+DOMAIN_NAME="prasads.site" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
 for instance in $@
