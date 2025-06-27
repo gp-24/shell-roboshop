ff9036088493a9267cc96019b2b78aea504b93b2
prasad <guruprasadbolisetty24@.com>
2025-06-27 10:36:38 +0530

    hello world started

--- roboshop.sh
+++ roboshop.sh
@@ -6,9 +6,9 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z0193002ZK4VQFXB1ARL" # replace with your ZONE ID
 DOMAIN_NAME="prasads.site" # replace with your domain
 
-for instance in ${INSTANCES[@]}
+for instance in $@ # instance in ${INSTANCES[@]}
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-06776cce51381651fsss --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-06776cce51381651f --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
